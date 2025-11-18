336cccfaa12283419fae1ebba77a354b9525413f
Mohammed Naser <MohammedNaser2826@gmai.com>
2025-11-18 22:58:04 +0200

    save edit yaml after restore

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -43,8 +43,8 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
+    Component.Graph(),
     Component.Backlinks(),
   ],
 }
